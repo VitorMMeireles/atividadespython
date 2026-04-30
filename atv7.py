c604050b7c9fb1aac40068d69abcf2a53356e29e
VitorMMeireles <melomeirelesvitor@gmail.com>
2026-04-30 11:22:38 -0300

    atividade 7 e 8

---
+++ atv7.py
@@ -0,0 +1,21 @@
+#questao07
+import os 
+os.system("cls")
+
+num1 = input("Digite o primeiro número: ")
+num2 = input("Digite o segundo número: ")
+
+num1 = float(num1)
+num2 = float(num2)
+
+result = num1 > num2
+
+print(result)
+
+
+
+"""if num1 > num2:
+    print("O segundo número é menor que o primeiro!")
+else:
+    print("O primeiro número é menor que o segundo!")
+"""
