70c03912bf8ac8b9c772aa58c29cd95bd4d76f10
VitorMMeireles <melomeirelesvitor@gmail.com>
2026-04-30 12:10:51 -0300

    aula completa 30/04

--- atv7.py
+++ atv7.py
@@ -8,7 +8,10 @@ num2 = input("Digite o segundo número: ")
 num1 = float(num1)
 num2 = float(num2)
 
-result = num1 > num2
+# um dos jeitos --> result = num1 > num2
+
+#operador ternário
+result = "num1 é maior" if num1 > num2 else "num1 é menor"
 
 print(result)
 
